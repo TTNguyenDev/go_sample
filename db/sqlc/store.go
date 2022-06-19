f4311ded1b1fbe70c1eab58705360dd1ba84fb6a
TTNguyenDev <nttriet9a4@gmail.com>
2022-06-19 20:56:26 +0700

    deadlock prevent

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -8,90 +8,116 @@ import (
 
 // Store provides all functions to execute db queries and transactions
 type Store struct {
-  *Queries
-  db *sql.DB
+	*Queries
+	db *sql.DB
 }
 
 func NewStore(db *sql.DB) *Store {
-  return &Store{
-    db: db,
-    Queries: New(db),
-  }
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
 }
 
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
-  tx, err := store.db.BeginTx(ctx, nil)
+	tx, err := store.db.BeginTx(ctx, nil)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  q := New(tx)
-  err = fn(q)
+	q := New(tx)
+	err = fn(q)
 
-  if err != nil {
-    if rbErr := tx.Rollback(); rbErr != nil {
-      return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
-    }
-    return err
-  }
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
 
-  return tx.Commit()
+	return tx.Commit()
 }
 
 // TransferTxParams contains the input parameters of the transfer transaction
 type TransferTxParams struct {
-  FromAccountId int64 `json:"from_account_id"`
-  ToAccountId int64 `json:"to_account_id"`
-  Amount int64 `json:"amount"`
+	FromAccountId int64 `json:"from_account_id"`
+	ToAccountId   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
 }
 
 // TransferTxResult is the result of the transfer transaction
 type TransferTxResult struct {
-  Transfer Transfer `json:"transfer"`
-  FromAccount Accounts `json:"from_account"`
-  ToAccount Accounts `json:"to_account"`
-  FromEntry Entries `json:"from_entry"`
-  ToEntry Entries `json:"to_entry"`
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Accounts `json:"from_account"`
+	ToAccount   Accounts `json:"to_account"`
+	FromEntry   Entries  `json:"from_entry"`
+	ToEntry     Entries  `json:"to_entry"`
 }
 
 // TransferTx performs a money transfer from one account to the other.
 // It create a transfer record, add account entries, and update accounts' balance within a single database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-  var result TransferTxResult
+	var result TransferTxResult
 
-  err := store.execTx(ctx, func(q *Queries) error {
-    var err error
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
 
-    result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams {
-      FromAccountID: arg.FromAccountId,
-      ToAccountID: arg.ToAccountId,
-      Amount: arg.Amount,
-    })
-    if err != nil {
-      return err
-    }
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountId,
+			ToAccountID:   arg.ToAccountId,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
-    result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams {
-      AccountID: arg.FromAccountId, 
-      Amount: -arg.Amount,
-    })
-    if err != nil {
-      return err
-    }
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountId,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
-    result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams {
-      AccountID: arg.ToAccountId, 
-      Amount: arg.Amount,
-    })
-    if err != nil {
-      return err
-    }
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountId,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
-    // TODO: update accounts' balance
+		// update accounts' balance
+		if arg.FromAccountId < arg.ToAccountId {
+			result.FromAccount, result.ToAccount, _ = addMoney(ctx, q, arg.FromAccountId, -arg.Amount, arg.ToAccountId, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, _ = addMoney(ctx, q, arg.ToAccountId, arg.Amount, arg.FromAccountId, -arg.Amount)
+		}
+		return nil
+	})
+	return result, err
+}
 
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64) (account1 Accounts, account2 Accounts, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
 
-    return nil
-  })
-  return result, err
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
+
 }
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -35,6 +35,7 @@ func TestTransferTx(t *testing.T) {
 
 
   // check results
+  existed := make(map[int]bool)
   for i:= 0; i < n; i++ {
     err := <- errs
     require.NoError(t, err)
@@ -74,7 +75,86 @@ func TestTransferTx(t *testing.T) {
 
     _, err = store.GetEntry(context.Background(), toEntry.ID)
     require.NoError(t, err)
+
+    // check accounts
+    fromAccount := result.FromAccount
+    require.NotEmpty(t, fromAccount)
+    require.Equal(t, account1.ID, fromAccount.ID)
+
+    toAccount := result.ToAccount
+    require.NotEmpty(t, toAccount)
+    require.Equal(t, account2.ID, toAccount.ID)
+
+    // check accounts' balance
+    diff1 := account1.Balance - fromAccount.Balance
+    diff2 := toAccount.Balance - account2.Balance
+    require.Equal(t, diff1, diff2)
+    require.True(t, diff1 > 0)
+    require.True(t, diff1 % amount == 0)
+
+    k := int(diff1 / amount)
+    require.True(t, k >= 1 && k <= n)
+    require.NotContains(t, existed, k)
+    existed[k] = true
+  }
+
+  // check the final updated balances
+  updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+  require.NoError(t, err)
+
+  updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+  require.NoError(t, err)
+
+  require.Equal(t, account1.Balance - int64(n)*amount, updatedAccount1.Balance)
+  require.Equal(t, account2.Balance + int64(n)*amount, updatedAccount2.Balance)
+}
+
+func TestTransferTxDeadlock(t *testing.T) {
+  store := NewStore(testDB)
+
+  account1 := createRandomAccount(t)
+  account2 := createRandomAccount(t)
+
+  n := 10 
+  amount := int64(10)
+
+  // channel creation
+  errs := make(chan error)
+
+  for i := 0; i < n; i++ {
+    fromAccountID := account1.ID
+    toAccountID := account2.ID
+
+    if i % 2 == 1 {
+      fromAccountID = account2.ID
+      toAccountID = account1.ID
+
+    }
+    go func() {
+      _, err := store.TransferTx(context.Background(), TransferTxParams{
+        FromAccountId: fromAccountID,
+        ToAccountId: toAccountID,
+        Amount: amount,
+      })
+
+      errs <- err
+    }()
   }
 
 
+  // check results
+  for i:= 0; i < n; i++ {
+    err := <- errs
+    require.NoError(t, err)
+  }
+
+  // check the final updated balances
+  updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+  require.NoError(t, err)
+
+  updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+  require.NoError(t, err)
+
+  require.Equal(t, account1.Balance, updatedAccount1.Balance)
+  require.Equal(t, account2.Balance, updatedAccount2.Balance)
 }
