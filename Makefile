dc932a5a81e1983198495683de72951fdf104ae3
TTNguyenDev <nttriet9a4@gmail.com>
2022-06-19 22:57:27 +0700

    restful http integration

--- Makefile
+++ Makefile
@@ -19,4 +19,7 @@ sqlc:
 test:
 	go test -v -cover ./...
 
-.PHONY: prostgres createdb dropdb migrateup migratedown sqlc test
+server:
+	go run main.go
+
+.PHONY: prostgres createdb dropdb migrateup migratedown sqlc test server
---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	db "github.com/TTNguyenDev/go_sample.git/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+  store *db.Store
+  router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+  server := &Server{store: store}
+  router := gin.Default()
+
+  // Add routing
+  router.POST("/accounts", server.createAccount)
+  router.GET("/accounts/:id", server.getAccount)
+  router.GET("/listAccounts", server.listAccounts)
+
+  server.router = router
+  return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+  return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+  return gin.H{"error": err.Error()}
+}
